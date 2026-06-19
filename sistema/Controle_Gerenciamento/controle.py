198c5d0eae0410719ff75ec55c377a438e9edc01
Maria Vasconcelos <marialuciavieiravvg@gmail.com>
2026-06-19 11:52:53 -0300

    ajeitando aqui

--- sistema/Controle_Gerenciamento/controle.py
+++ sistema/Controle_Gerenciamento/controle.py
@@ -1,4 +1,5 @@
 import mysql.connector as mysql_connector
+import os
 
 banco = mysql_connector.connect(
     host="localhost",
@@ -7,5 +8,34 @@ banco = mysql_connector.connect(
     database="lanchonete"
 )
 
+# Atributos públicos
+
+
+# Sistema começa aqui
 def funcao_principal():
     print(f"Entrando no sistema da Lanchonete")
+
+    while (True):
+        # MENU
+        print("------------Menu------------")  
+        print("[1] - Cadastrar Cliente")
+        print("[2] - Cadastrar Funcionário")  
+        print("[3] - Fazer Login")
+        print("[4] - Sair")
+        opcao = int(input("Digite a opção desejada: "))
+
+        if opcao == 1:
+            nome = str(input("Digite seu nome: "))
+            cpf = str(input("Digite seu CPF: "))
+            telefone = str(input("Digite seu número (formato: (XX) XXXXX-XXXX): "))
+
+            cursor = banco.cursor()
+            comando_SQL = "INSERT INTO cliente (telefone, cpf, nome) VALUES (%s, %s, %s)"
+            dados = (str(telefone), str(cpf), str(nome))
+            cursor.execute(comando_SQL, dados)
+            banco.commit()
+            os.system('cls')
+
+
+
+funcao_principal()
